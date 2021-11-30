cf657756059d8d292535b746edb7a821aa06b024
Raj-04 <1204patel.raj@gmail.com>
2021-11-30 16:28:43 -0500

    can get all the posts

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,20 @@
 // implement your posts router here
+const express = require('express')
+const Posts = require('./posts-model')
+const router = express.Router()
+
+// [GET] /api/posts
+router.get('/', (req, res) => {
+    Posts.find()
+        .then(post => {
+            res.status(200).json(post)
+        })
+        .catch(error => {
+            res.status(500).json({
+                message: 'The posts information could not be retrieved',
+                error: error.message,
+            })
+        })
+})
+
+module.exports = router
