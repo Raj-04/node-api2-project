8490971d55d1d9dc1c4e121663302593311a03b5
Raj-04 <1204patel.raj@gmail.com>
2021-11-30 16:51:54 -0500

    responds with updated user

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -61,4 +61,37 @@ router.post('/', (req, res) => {
     }
 })
 
+// [PUT] /api/posts/:id
+router.put('/:id', async (req, res) => {
+    const { id } = req.params
+    const change = req.body
+    if(!change.title || !change.contents) {
+        return res.status(400).json({
+            message: 'Please provide title and contents for the post'
+        })
+    }
+    Posts.findById(id)
+        .then((selectedPost) => {
+            if(!selectedPost) {
+                res.status(404).json({
+                    message: 'The post with the specified ID does not exist' 
+                })
+            } else {
+                return Posts.update(id, change)
+            }
+        })
+        .then((canUpdate) => {
+            if(canUpdate) {
+                res.status(201).json({
+                    ...change,
+                    id: +id
+                })
+            }
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+})
+
+
 module.exports = router
