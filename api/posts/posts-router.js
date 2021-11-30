d2edf31c822e03378cfcba712bf4225ad98ea2d7
Raj-04 <1204patel.raj@gmail.com>
2021-11-30 16:32:35 -0500

    can get all posts by id

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -17,4 +17,24 @@ router.get('/', (req, res) => {
         })
 })
 
+// [GET] /api/posts/:id
+router.get('/:id', (req, res) => {
+    const { id } = req.params
+    Posts.findById(id)
+        .then(post => {
+            if(!post) {
+                res.status(404).json({
+                    message: 'The post with the specified ID does not exist'
+                })
+            } else {
+                res.status(200).json(post)
+            }
+        })
+        .catch(error => {
+            res.status(500).json({
+                message: 'The post information could not be retrieved'
+            })
+        })
+})
+
 module.exports = router
