a491c0110e06f7556c86a002aa587eb5f44dbcd3
Raj-04 <1204patel.raj@gmail.com>
2021-11-30 16:57:00 -0500

    can delete a post

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -93,5 +93,32 @@ router.put('/:id', async (req, res) => {
         })
 })
 
+// [DELETE] /api/posts/:id
+router.delete('/:id', (req, res) => {
+    Posts.findById(req.params.id)
+        .then((post) => {
+            if(!post) {
+                res.status(404).json({
+                    message: 'The post with the specified ID does not exist'
+                })
+            } else {
+                Posts.remove(req.params.id)
+                .then((id) => {
+                    res.status(200).json(post)
+                })
+                .catch((error) => {
+                    res.status(500).json({
+                        message: 'The post could not be removed'
+                    })
+                })
+            }
+        })
+        .catch((error) => {
+            res.status(500).json({ 
+                message: 'The post could not be removed'
+            })
+        })
+})
+
 
 module.exports = router
